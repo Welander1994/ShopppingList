a4d9cad37d77d02a1064056a85d1757e22a048c8
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2021-02-05 09:14:56 +0100

    shoppingApp

---
+++ index.js
@@ -0,0 +1,42 @@
+
+var shoppingList = new Vue({
+    el: "#shoppingList",
+    data: {
+        newItem: "",
+        shoppingList: [
+            {name: "milk", collected: false},
+            {name: "bread", collected: false}
+        ],
+    },
+
+    methods: {
+        clearAll: function() {
+            this.shoppingList = []
+            this.save()
+        },
+        deleteItem: function(key) {
+            this.shoppingList.splice(key, 1)
+            this.save()
+            console.log(key);
+        },
+        addItem: function(e) {
+            
+            if(this.newItem !== ""){
+                this.shoppingList.push({
+                    name: this.newItem,
+                    collected: false,
+                })
+                this.newItem = ""
+                this.save()
+            }
+        },
+        save: function() {
+            localStorage.shoppingList = JSON.stringify(this.shoppingList)
+        },
+    },
+    mounted() {
+        if(localStorage.shoppingList){
+            this.shoppingList = JSON.parse(localStorage.shoppingList)
+        }
+    },
+})
